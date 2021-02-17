227e869f90680dd8a27799f387d5bfb3ceced5ea
Jussi Heinonen <heinonen.jussi@gmail.com>
2021-02-17 15:03:04 +0000

    Enable command line arguments processing Enables to specify WSGI start directory

--- wiren/entrypoint.sh
+++ wiren/entrypoint.sh
@@ -2,12 +2,59 @@
 #
 # Start serverless-dynamodb-local before starting web server 
 #
-# USAGE: ./entrypoint.sh 
+# USAGE: ./entrypoint.sh --start_dynamodb=true --start_wsgi=true --wsgi_dir=../core/core
 
 export USERS_TABLE="users-table-dev"
 export IS_OFFLINE="true"
 
+info() {
+  echo -e "\e[34m ${1}\e[0m"
+}
+
+printCliArgs() {
+  for each in "${!ARGS[@]}"
+  do
+    echo "ARGS[${each}]=${ARGS[${each}]}"
+  done
+}
+
+processCliArgs() {
+  #  Reads arguments into associative array ARGS[]
+  #  Key-Value argument such as --myarg="argvalue" adds an element ARGS[--myarg]="argvalue"
+  #
+  #  USAGE: processCliArgs $*
+  for each in $*; do
+    if [[ "$(echo ${each} | grep '=' >/dev/null ; echo $?)" == "0" ]]; then
+      key=$(echo ${each} | cut -d '=' -f 1)
+      value=$(echo ${each} | cut -d '=' -f 2)
+      if [[ "${ARGS[--debug]}" ]]; then
+        if [[ "${key}" =~ "key" ]]; then
+          info "Processing Key-Value argument ${key}=${value:0:4}********************"
+        else
+          info "Processing Key-Value argument ${key}=${value}"
+        fi
+      fi
+      ARGS[${key}]="${value}"
+    else
+      errorAndExit "$0.${FUNCNAME}: Argument must contain = character as in --key=value" 1
+    fi
+  done
+}
+
+declare -A ARGS
+processCliArgs $*
+test -z ${ARGS[--start_dynamodb]} && ARGS[--start_dynamodb]="true"
+test -z ${ARGS[--start_wsgi]} && ARGS[--start_wsgi]="true"
+test -z ${ARGS[--wsgi_dir]} && ARGS[--wsgi_dir]="./$(dirname $0)"
+printCliArgs
+
 cd ./$(dirname $0)
 sls dynamodb start &
 sleep 3
-sls wsgi serve
+
+if [[ "${ARGS[--start_wsgi]}" == "true" ]]; then
+    cd ${ARGS[--wsgi_dir]}
+    sls wsgi serve
+else
+    info "--start_wsgi flag set to ${ARGS[--start_wsgi]}"
+fi
