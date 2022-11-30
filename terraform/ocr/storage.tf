323b4daac75cbc8afb7ebcf3dd55602a340a069e
Jussi Heinonen <heinonen.jussi@gmail.com>
2022-11-30 18:15:59 +0000

    Enable server-side encryption on the bucket

--- terraform/ocr/storage.tf
+++ terraform/ocr/storage.tf
@@ -9,3 +9,13 @@ resource "aws_s3_bucket_acl" "ocr_bucket_inbound_acl" {
   bucket = aws_s3_bucket.ocr_bucket_inbound.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "ocr_bucket_inbound_enc" {
+  bucket = aws_s3_bucket.ocr_bucket_inbound.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
