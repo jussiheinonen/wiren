32bb08091b73dcb52dd4370ba37bdbb53287005e
Jussi Heinonen <heinonen.jussi@gmail.com>
2022-12-13 16:42:47 +0000

    Add config for kyc.json from source file

--- terraform/kyc/storage.tf
+++ terraform/kyc/storage.tf
@@ -20,9 +20,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "kyc_bucket_outbou
 }
 
 resource "aws_s3_object" "kyc_bucket_outbound_object" {
-  bucket  = aws_s3_bucket.kyc_bucket_outbound.id
-  key     = "kyc.json"
-  content = jsonencode({
+  count       = local.kyc.source_file == false ? 1 : 0
+  depends_on  = [aws_s3_bucket.kyc_bucket_outbound]
+  bucket      = aws_s3_bucket.kyc_bucket_outbound.id
+  key         = "kyc.json"
+  content     = jsonencode({
     "description": local.kyc.verification_description,
     "criteria": {
       "residence": local.kyc.verification.residence,
@@ -31,4 +33,13 @@ resource "aws_s3_object" "kyc_bucket_outbound_object" {
       "identity": local.kyc.verification.identity
     }
   })
+}
+
+resource "aws_s3_object" "kyc_bucket_outbound_object_from_json" {
+  count       = local.kyc.source_file == false ? 0 : 1
+  depends_on  = [aws_s3_bucket.kyc_bucket_outbound]
+  bucket      = aws_s3_bucket.kyc_bucket_outbound.id
+  key         = "kyc.json"
+  source      = local.kyc.source_file
+  etag        = filemd5(local.kyc.source_file)
 }
